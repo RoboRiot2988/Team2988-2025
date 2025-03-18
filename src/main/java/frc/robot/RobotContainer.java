5090429b76a98120e45cd9cba05997e1a2c9bbb7
Jonathan Edwards <jonathan.edwards.ee@gmail.com>
2025-03-18 10:39:15 -0400

    Add Coral Spin test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -230,7 +230,8 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Auto");
+    // return drivebase.getAutonomousCommand("New Auto");
+    return drivebase.getAutonomousCommand("Coral Test");
   }
 
   public void setMotorBrake(boolean brake)
